1f89d6aac9e6aaf9bd15cd987a9b1fdddd20619f
Megan Sorour <srrmeg001@myuct.ac.za>
2023-09-11 23:12:53 +0200

    Script added which can run whole Sim at once

--- Project Files/CompleteSim.m
+++ Project Files/CompleteSim.m
@@ -1,16 +1,26 @@
+% complete script for the Simulation
+%initialising variables before simulation runs
+samplef = (2.5 * fs);
+mic_src_xy = abs(m - src);  % diff in xy coords on each mic and src
+mic_src_d = sqrt(sum(mic_src_xy .^2, 2)) % distance between each mic and src
+speed = 343.21; %speed of sound at T = 20C
+delay = mic_src_d ./ speed
+
+%running the simulation, outputs assigned to out
+out = sim('SimPart1.slx');
+
+%signal processing algorithm
 sim_time = 2;
 dt = 1/samplef;
 t = 0:dt:sim_time ;
 t = t';
 
-N = 1;
+N = 1; %order of the Butterworth filter
 low_freq = fs-100;
 high_freq = fs+100;
 nlow_f = low_freq/samplef/2;
 nhigh_f = high_freq/samplef/2;
-%Wn = [low_freq, high_freq] ./ (samplef / 2);
 Wn = [nlow_f, nhigh_f];
-%Wn = [0.19, 0.21];
 
 % % Design the Butterworth bandpass filter
 [B, A] = butter(N, Wn , 'bandpass');
@@ -20,21 +30,12 @@ spm2 = filter(B, A, out.m2out);
 spm3 = filter(B, A, out.m3out);
 spm4 = filter(B, A, out.m4out);
 
-% Plot the filtered signal
-% tiledlayout(2,1)
-% nexttile
-% plot(t, out.m1out)
-% title("No filter")
-% 
-% nexttile
-% plot(t, spm1);
-% title('Filtered Signal');
+% m1filtered = cat(2, t, spm1);
+% m2filtered = cat(2, t, spm2);
+% m3filtered = cat(2, t, spm3);
+% m4filtered = cat(2, t, spm4);
 
-m1filtered = cat(2, t, spm1);
-m2filtered = cat(2, t, spm2);
-m3filtered = cat(2, t, spm3);
-m4filtered = cat(2, t, spm4);
-
+%expected tdoa values
 td12 = delay(2) - delay(1);
 td13 = delay(3) - delay(1);
 td14 = delay(4) - delay(1);
@@ -70,3 +71,13 @@ syms x y;
 % x
 % y
 
+% all plots for debugging / report
+% Signal processing results
+% tiledlayout(2,1)
+% nexttile
+% plot(t, out.m1out)
+% title("Original signal from mic 1")
+% 
+% nexttile
+% plot(t, spm1);
+% title('Filtered signal from mic 1');
