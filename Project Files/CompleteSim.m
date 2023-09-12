f956b3f628a3e02fd67de9bac71ed3d9d883f85a
Megan Sorour <srrmeg001@myuct.ac.za>
2023-09-12 11:51:30 +0200

    Struggle

--- Project Files/CompleteSim.m
+++ Project Files/CompleteSim.m
@@ -1,13 +1,32 @@
 % complete script for the Simulation
-%initialising variables before simulation runs
+% Define variables:
+
+%positions of mics
+m1 = [0, 0];
+m2 = [0, 0.5];
+m3 = [0.8, 0.5];
+m4 = [0.8, 0];
+m = [m1;m2;m3;m4];
+
+src = [0.6, 0.4]; %location of sound source
+
+fs = 2000; %frequency of source
+amp = 10; %amplitude of source (volume)
+
+np = 0; %noise power
+
+%calculating derived variables before simulation runs
 samplef = (2.5 * fs);
 mic_src_xy = abs(m - src);  % diff in xy coords on each mic and src
-mic_src_d = sqrt(sum(mic_src_xy .^2, 2)) % distance between each mic and src
+mic_src_d = sqrt(sum(mic_src_xy .^2, 2)); % distance between each mic and src
 speed = 343.21; %speed of sound at T = 20C
-delay = mic_src_d ./ speed
+delay = mic_src_d ./ speed;
+
+disp(['Coordinates of sound source [x, y]: [' num2str(src(1)) ', ' num2str(src(2)) ']' ]);
 
 %running the simulation, outputs assigned to out
 out = sim('SimPart1.slx');
+%out = sim('SimPart1a.slx'); %for older MATLAB versions
 
 %signal processing algorithm
 sim_time = 2;
@@ -35,26 +54,36 @@ spm4 = filter(B, A, out.m4out);
 % m3filtered = cat(2, t, spm3);
 % m4filtered = cat(2, t, spm4);
 
+% Signal processing results
+% tiledlayout(2,1)
+% nexttile
+% plot(t, out.m1out)
+% title("Original signal from mic 1")
+% 
+% nexttile
+% plot(t, spm1);
+% title('Filtered signal from mic 1');
+
 %expected tdoa values
 td12 = delay(2) - delay(1);
 td13 = delay(3) - delay(1);
 td14 = delay(4) - delay(1);
-
-%-----------------------------------------------------------------------
-
-%TDoA function call 3x
+% 
+% %-----------------------------------------------------------------------
+% 
+% %TDoA function call 3x
 disp(['Expected result: ' num2str(td12) ' seconds']);
 TDoA = TDoAFunction(spm2, spm1, samplef);
 disp(['Estimated TDoA between M1 and M3: ' num2str(TDoA) ' seconds']);
-
-disp('---------------------------------------------------------------');
-
+% 
+% disp('---------------------------------------------------------------');
+% 
 disp(['Expected result: ' num2str(td13) ' seconds']);
 TDoA = TDoAFunction(spm3, spm1, samplef);
 disp(['Estimated TDoA between M1 and M3: ' num2str(TDoA) ' seconds']);
-
-disp('---------------------------------------------------------------');
-
+% 
+% disp('---------------------------------------------------------------');
+% 
 disp(['Expected result: ' num2str(td14) ' seconds']);
 TDoA = TDoAFunction(spm4, spm1, samplef);
 disp(['Estimated TDoA between M1 and M4: ' num2str(TDoA) ' seconds']);
@@ -70,14 +99,3 @@ syms x y;
 % [x, y] = LocalizationFunction(td12, td13, td14);
 % x
 % y
-
-% all plots for debugging / report
-% Signal processing results
-% tiledlayout(2,1)
-% nexttile
-% plot(t, out.m1out)
-% title("Original signal from mic 1")
-% 
-% nexttile
-% plot(t, spm1);
-% title('Filtered signal from mic 1');
